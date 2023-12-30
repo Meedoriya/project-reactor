285c234c851f274377503732bdb029b836d5d0c9
Meedoriya <meedoriya@gmail.com>
2023-12-30 15:54:29 +0500

    first commit

---
+++ src/test/java/com/alibi/reactor/CatalizatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alibi.reactor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalizatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
